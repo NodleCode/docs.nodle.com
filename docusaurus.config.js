1d52277f4254bba2117bf5d5cfb6b76eaeb7ea73
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2022-02-21 17:19:46 -0300

    fixed components responsiveness

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,12 +58,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          { to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          { to: '/blog', label: 'Blog', position: 'left'}
         ],
       },
       footer: {
